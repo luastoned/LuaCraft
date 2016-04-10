1b678711edfd929f9d5d0f5a5665f6dace19fb8c
fr1kin <matt.fr1kin@gmail.com>
2016-04-09 19:32:01 -0500

    Added some extra methods for plugin support and comments on all methods

--- main/java/com/luacraft/classes/FileMount.java
+++ main/java/com/luacraft/classes/FileMount.java
@@ -9,6 +9,11 @@ public class FileMount {
 	private static File mountedRoot;
 	private static ArrayList<File> mounted = new ArrayList<File>();
 
+	/**
+	 * Removes the root from a files path
+	 * @param file File to get path of
+	 * @return Path without root dir
+	 */
 	public static String CleanPath(File file) {
 		String clean = file.getPath();
 
@@ -18,20 +23,48 @@ public class FileMount {
 		return file.getName();
 	}
 
+	/**
+	 * Creates directories if they don't exist
+	 * @param path Directories to create
+	 */
 	public static void CreateDirectories(String path) {
 		File file = GetFileInMountedRoot(path);
 		if(!file.exists())
 			file.mkdirs();
 	}
 
+	/**
+	 * Set the root folder that will contain the lua folders
+	 * Leave empty if you just want to use the default root dir
+	 * @param path Name of the root dir
+	 */
 	public static void SetMountedRoot(String path) {
 		mountedRoot = GetFileInRoot(path);
 	}
 
+	/**
+	 * Gets the mounted root
+	 * @return null if it hasn't been set
+	 */
+	public static File GetMountedRoot() {
+		return mountedRoot;
+	}
+
+	/**
+	 * Get file relative to the root dir
+	 * @param file File name
+	 * @return File object
+	 */
 	public static File GetFileInRoot(String file) {
 		return new File(LuaCraft.rootDir, file);
 	}
 
+	/**
+	 * Get file relative to the mounted root
+	 * If the mounted root was never set, it will just use the normal root
+	 * @param file File name
+	 * @return File object
+	 */
 	public static File GetFileInMountedRoot(String file) {
 		if(mountedRoot == null) {
 			return GetFileInRoot(file);
@@ -40,6 +73,10 @@ public class FileMount {
 		}
 	}
 
+	/**
+	 * Add directory to the mount
+	 * @param path Directory name
+	 */
 	public static void MountDirectory(String path) {
 		File dir = GetFileInMountedRoot(path);
 
@@ -49,14 +86,19 @@ public class FileMount {
 		mounted.add(dir);
 	}
 
+	/**
+	 * Gets list of current mounted directories
+	 * @return copied list
+	 */
 	public static ArrayList<File> GetMountedDirectories() {
 		return new ArrayList<>(mounted);
 	}
 
-	public static File GetMountedRoot() {
-		return mountedRoot;
-	}
-
+	/**
+	 * Gets a file in first the root dir, then a mounted dir if one didn't exist in the root
+	 * @param file File name
+	 * @return File object
+	 */
 	public static File GetFile(String file) {
 		File rootFile = GetFileInMountedRoot(file);
 
@@ -72,6 +114,11 @@ public class FileMount {
 		return null;
 	}
 
+	/**
+	 * Gets a list of the files in both the root and then the mounted directories
+	 * @param folder Folder containing the files
+	 * @return List of files
+	 */
 	public static ArrayList<File> GetFilesIn(String folder) {
 		ArrayList<File> files = new ArrayList<File>();
 
