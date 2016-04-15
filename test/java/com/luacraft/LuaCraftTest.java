69f3dde440382950ac701a726ac44de34c77b534
fr1kin <matt.fr1kin@gmail.com>
2016-04-14 21:20:13 -0500

    Removed test folder

--- test/java/com/luacraft/LuaCraftTest.java
+++
@@ -1,9 +0,0 @@
-package com.luacraft;
-
-/**
- * com.luacraft
- * <p>
- * Created on 4/11/2016 by Matthew
- */
-public class LuaCraftTest {
-}
--- test/java/com/luacraft/classes/FileMountTest.java
+++
@@ -1,9 +0,0 @@
-package com.luacraft.classes;
-
-/**
- * com.luacraft.classes
- * <p>
- * Created on 4/11/2016 by Matthew
- */
-public class FileMountTest {
-}
